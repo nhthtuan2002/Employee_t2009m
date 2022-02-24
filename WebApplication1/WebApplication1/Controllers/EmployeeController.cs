6ce463327546240687d35dc776bc529574a63572
Tuấn Nguyễn <hoangtuandz2002@gmail.com>
2022-02-24 12:59:37 +0700

    E nop bai

---
+++ WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ServiceEAP;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        static ServiceEAP.Service1Client service1 = new ServiceEAP.Service1Client();
+        // GET: Employee
+        public ActionResult Index(string search)
+        {
+            return View(service1.ListEmployee(search));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Save(Employee employee)
+        {
+            service1.CreateEmployee(employee);
+            return Redirect("Index");
+        }
+    }
+}
